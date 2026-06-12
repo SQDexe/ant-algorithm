4577502cf9531f8455da22bbdb33842da81a6c89
Jakub <qba.szu@gmail.com>
2026-06-12 12:40:06 +0200

    Slight change to descriptions

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 A university project - an implementation of Ant Colony Optimisation Alogrithm, packaged with a small CLI tool for inspecting results, allows setting some parameters on the run, with the main route backed into the executable
 
 ## Acknowledgements
-Based on 'Klasyczny algorytm mrówkowy v.2.0' by Feliks Kurp
+Based on `Klasyczny algorytm mrówkowy v.2.0` by Feliks Kurp
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 A university project - an implementation of Ant Colony Optimisation Alogrithm, packaged with a small CLI tool for inspecting results, allows setting some parameters on the run, with the main route backed into the executable
 
 ## Acknowledgements
-Based on 'Klasyczny algorytm mrówkowy v.2.0' by Feliks Kurp
+Based on `Klasyczny algorytm mrówkowy v.2.0` by Feliks Kurp
 */
 
 /** **Logic module** - ants managment and logic. */
